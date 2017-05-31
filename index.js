e519a3b81bb0f2e423bd6d1190dfdcfbbc3c3684
vadimreznik <vadim.reznik@hotmail.com>
2017-06-01 00:14:23 +0300

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,134 @@
 window.onload = init;
 
 function init(){
-	document.getElementById('generate').onclick = generate;
+	document.getElementById('generate').onclick = generateCombinations;
+	document.getElementById('pins').onkeyup = buildPinsHeight;
+	document.getElementById('pairs').onchange = removeDublicatePairs;
+	document.getElementById('alternate').onchange = removeAlternatePairs;
+}
+
+function removeDublicatePairs(){
+	if(allValues && !!allValues.length){
+		isCheckedDublicate = this.checked;
+		if(this.checked){
+			woDublicates = isCheckedAlternate ? woAlternate.slice() : allValues.slice();
+
+			woDublicates = woDublicates.filter(function(item){ return isKeyHasValidPairs(item.split('')); });
+
+			postResult('', true);
+			postResult('Values wo dublicates: ' + woDublicates.length);
+
+			woDublicates.forEach(function(item){
+				postResult(item);
+			});
+		} else {
+			var arr = isCheckedAlternate ? woAlternate.slice() : allValues.slice();
+
+			postResult('', true);
+			postResult(isCheckedAlternate ? 'Values wo alternate: ' : 'All possible values: ' + arr.length);
+
+			arr .forEach(function(item){
+				postResult(item);
+			});
+		}
+	}
+}
+
+function removeAlternatePairs(){
+	if(allValues && !!allValues.length){
+		isCheckedAlternate = this.checked;
+		if(this.checked){
+			woAlternate = isCheckedDublicate ? woDublicates.slice() : allValues.slice();
+
+			woAlternate = woAlternate.filter(function(item){ return isKeyHasAlternatePairs(item.split('')); });
+
+			postResult('', true);
+			postResult('Values wo alternate: ' + woAlternate.length);
+
+			woAlternate.forEach(function(item){
+				postResult(item);
+			});
+		} else {
+			var arr = isCheckedDublicate ? woDublicates.slice() : allValues.slice();
+
+			postResult('', true);
+			postResult(isCheckedDublicate ? 'Values wo dublicates: ' : 'All possible values: ' + arr.length);
+
+			arr .forEach(function(item){
+				postResult(item);
+			});
+		}
+	}
+}
+
+function buildPinsHeight(){
+	var heights = document.getElementById('heights');
+	var val = Number(this.value);
+	if(isNaN(val)){
+		postResult('Value is not a number!', true);
+	} else {
+		postResult('', true);
+	}
+	if(val > 12){
+		this.value = 12;
+		val = this.value;
+	}
+	var model = [];
+	for(var i = 1; i <= val; i++){
+		model.push(i);
+	}
+	
+	heights.innerHTML = '<div><strong>Enter the height of the each pin (in mm.):</strong></div>' + Template('<div class="cell"><label>Pin |item|</label><input type="text" id="id|item|"></div>')(model);
+	heights.removeEventListener('keyup', gatheringHeights);
+	heights.addEventListener('keyup', gatheringHeights);
+}
+
+function validateNumber(val){
+	return !isNaN(Number(val));
+}
+
+var storage = {};
+var allValues, woDublicates, woAlternate;
+var isCheckedDublicate = false;
+var isCheckedAlternate = false;
+
+function gatheringHeights(e){
+	if(validateNumber(e.target.value)){
+		storage[e.target.id] = e.target.value;
+	} else {
+		e.target.value = '';
+	}
+}
+
+function generateCombinations(){
+	var pinsCount = Number(document.getElementById('pins').value);
+	var pins = Object.keys(storage);
+
+	if(pins.length === pinsCount){
+		var pinsArray = [];
+		var hasValues = pins.every(function(key){
+			if(storage[key] !== undefined){
+				pinsArray.push(storage[key]);
+			}
+			return storage[key] !== undefined;
+		});
+		if(hasValues){
+			allValues = combinations({str:pinsArray.join('')});
+			
+			postResult('All possible values: ' + allValues.length);
+			allValues.forEach(function(item){
+				postResult(item);
+			});
+
+			document.getElementById('controls').style.display = 'block';
+			document.getElementById('generate').style.display = 'none';
+
+		} else {
+			postResult('Some pin is absent!', true);
+		}
+	} else {
+		postResult('Something went wrong!', true);
+	}
 }
 
 function generate(){
@@ -38,19 +165,24 @@ function generate(){
 	postResult('All possible values: ' + allResults.length);
 	postResult('Unique values: ' + uniqueResults.length);
 
+	postResult('-------------');
+	allResults.forEach(function(item){
+		postResult(item);
+	});
+
 	if(window.location.search === '?show'){
 		postResult('-------------');
 		uniqueResults.forEach(function(item){
 			postResult(item);
 		});
 	}
+}
 
-	function postResult(msg, clean){
-		if(clean){
-			node.result.innerHTML = msg;
-		} else {
-			node.result.innerHTML += msg + '<br>';
-		}
+function postResult(msg, clean){
+	if(clean){
+		document.getElementById('result').innerHTML = msg;
+	} else {
+		document.getElementById('result').innerHTML += msg + '<br>';
 	}
 }
 
@@ -64,6 +196,10 @@ function possiblePinValues(min, max){
 
 function isPinEquals(arr, i){
 	return arr[i - 1] === undefined ? false : arr[i - 1] === arr[i];
+}
+
+function isAlternatePinEquals(arr, i){
+	return arr[i - 1] === undefined || arr[i + 1] === undefined ? false : arr[i - 1] === arr[i + 1];
 }
 
 function isStepSizeValid(arr, i){
@@ -84,6 +220,24 @@ function verify(arr, i){
 	}
 }
 
+function isKeyHasValidPairs(pins){
+	for(var i = 0; i < pins.length; i++){
+		if(isPinEquals(pins, i)){
+			return false;
+		}
+	}
+	return true;
+}
+
+function isKeyHasAlternatePairs(pins){
+	for(var i = 0; i < pins.length; i++){
+		if(isAlternatePinEquals(pins, i)){
+			return false;
+		}
+	}
+	return true;
+}
+
 function isKeyValid(pins){
 	for(var i = 0; i < pins.length; i++){
 		if(!verify(pins, i)){
@@ -100,3 +254,130 @@ function LoopIt(depth, baseString, arrLetters) {
 	}
   return returnValue;
 }
+
+function Template(html){
+    return function (obj) {
+        var key, text = '';
+        if (isArray(obj)) {
+            obj.forEach(function (item) {
+                text += html.replace(/\|([a-z]+)*\|/gim, function (match, key) {
+                    return isObject(item) ? item[key] : item;
+                });
+            });
+            return text;
+        } else if (isObject(obj)) {
+            return html.replace(/\|([a-z]+)*\|/gim, function (match, key) {
+                return obj[key] || '';
+            });
+        }
+    };
+}
+
+function isExists(item){
+	return !!item;
+}
+
+function isArray(item){
+    if (isExists(item)) {
+        if (item instanceof Array) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function isObject(item){
+    if (isExists(item)) {
+        if (!isArray(item) && item instanceof Object) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+
+
+/*
+Here are some examples of how you could use this code:
+  alert(combinations({str: "ABC"}).join(", "));
+  Displays:
+  AAA, AAB, AAC, ABA, ABB, ABC, ACA, ACB, ACC,
+  BAA, BAB, BAC, BBA, BBB, BBC, BCA, BCB, BCC,
+  CAA, CAB, CAC, CBA, CBB, CBC, CCA, CCB, CCC
+or
+  alert(combinations({arr: ["AB","CD","EF"]}).join(", "));
+  Displays:
+  ACE, ACF, ADE, ADF, BCE, BCF, BDE, BDF
+*/
+function combinations(args) {
+  var n, inputArr = [], copyArr = [], results = [],
+  subfunc = function(copies, prefix) {
+    var i, myCopy = [], exprLen, currentChar = "", result = "";
+    // if no prefix, set default to empty string
+    if (typeof prefix === "undefined") {
+      prefix = "";
+    }
+    // no copies, nothing to do... return
+    if (!isArray(copies) || typeof copies[0] === "undefined") {
+      return;
+    }
+    // remove first element from "copies" and store in "myCopy"
+    myCopy = copies.splice(0, 1)[0];
+    // store the number of characters to loop through
+    exprLen = myCopy.length;
+    for (i = 0; i < exprLen; i += 1) {
+      currentChar = myCopy[i];
+      result = prefix + currentChar;
+      // if resulting string length is the number of characters of original string,
+      // we have a result
+      if (result.length === n) {
+        results.push(result);
+      }
+      // if there are copies left,
+      //   pass remaining copies (by value) and result (as new prefix)
+      //   into subfunc (recursively)
+      if (typeof copies[0] !== "undefined") {
+        subfunc(copies.slice(0), result);
+      }
+    }
+  };
+  // for each character in original string
+  //   create array (inputArr) which contains original string (converted to array of char)
+  if (typeof args.str === "string") {
+    inputArr = args.str.split("");
+    for (n = 0; n < inputArr.length; n += 1) {
+      copyArr.push(inputArr.slice(0));
+    }
+  }
+  if (isArray(args.arr)) {
+    for (n = 0; n < args.arr.length; n += 1) {
+      copyArr.push(args.arr[n].split(""));
+    }
+  }
+  // pass copyArr into sub-function for recursion
+  subfunc(copyArr);
+  return results;
+};
