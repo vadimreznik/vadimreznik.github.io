a520654d36cc5af45e9a2aafceeec124cdbb416a
vadimreznik <vadym.riznyk@globallogic.com>
2024-01-09 13:28:43 +0100

    test 6

--- js/main.js
+++ js/main.js
@@ -31,32 +31,10 @@ window.onload = () => {
     }
     // start();
 }
-
-const debounce = (func, wait) => {
-    let timeout;
-    return function executedFunction(...args) {
-      const later = () => {
-        clearTimeout(timeout);
-        func(...args);
-      };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-    };
-  };
   
   document.documentElement.style.setProperty(
     "--100vh",
     `${window.innerHeight}px`
-  );
-  
-  window.addEventListener(
-    "resize",
-    debounce(() => {
-      document.documentElement.style.setProperty(
-        "--100vh",
-        `${window.innerHeight}px`
-      );
-    }, 100)
   );
 
   demo();
