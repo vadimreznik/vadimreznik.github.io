6f65a00398a3bc32647eb3f906b1847924d997ed
vadimreznik <vadym.riznyk@globallogic.com>
2024-01-09 12:30:08 +0100

    footer 6

--- js/main.js
+++ js/main.js
@@ -32,6 +32,33 @@ window.onload = () => {
     start();
 }
 
+const debounce = (func, wait) => {
+    let timeout;
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  };
+  
+  document.documentElement.style.setProperty(
+    "--100vh",
+    `${window.innerHeight}px`
+  );
+  
+  window.addEventListener(
+    "resize",
+    debounce(() => {
+      document.documentElement.style.setProperty(
+        "--100vh",
+        `${window.innerHeight}px`
+      );
+    }, 100)
+  );
+
 const hashCode = s => s.split('').reduce((a, b) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
 
 function calculateGameTime() {
